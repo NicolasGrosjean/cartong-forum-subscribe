fdf0490572097e1027125eefade5dbd0e45b04a7
NicolasGrosjean <grosjen@hotmail.fr>
2025-01-25 18:23:24 +0100

    Add a subscription form

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,17 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="S'inscrire au forum de CartONG" />
+        <input type="text" placeholder="Saisir votre email ici" />
+        <button onClick={async () => {
+          const email = document.querySelector('input').value;
+          const response = await fetch(`/.netlify/functions/subscribe?email=${email}`);
+          if (response.ok) {
+            alert('Inscription réussie, un email de confirmation vous a été envoyé.');
+          } else {
+            alert('Echec de l\'inscription, veuillez envoyer un email aux administrateurs du forum.');
+          }
+        }}>S'inscrire</button>
       </main>
 
       <Footer />
